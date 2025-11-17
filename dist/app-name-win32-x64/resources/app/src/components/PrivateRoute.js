f00974f71a2be1c9963eebdd2d60744a24ca844b
kevin <mrsek2022@gmail.com>
2025-11-17 17:13:43 -0500

    Ignorar dist y binarios

--- dist/app-name-win32-x64/resources/app/src/components/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-// src/components/PrivateRoute.js
-import { Navigate } from 'react-router-dom';
-
-const PrivateRoute = ({ children }) => {
-  const token = localStorage.getItem('token');
-  return token ? children : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
